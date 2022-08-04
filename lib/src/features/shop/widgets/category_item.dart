f8b6d25786051cafe4e371e13a948eff7829a896
Baus-san <edzonbausa33@gmail.com>
2022-08-04 13:38:38 +0800

    Added Animation on Expanded State of CategoryItem widget

--- lib/src/features/shop/widgets/category_item.dart
+++ lib/src/features/shop/widgets/category_item.dart
@@ -20,58 +20,67 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    // TODO: Animate Expanding State
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        InkWell(
-          onTap: onTap,
-          child: Ink(
-            height: 96,
-            padding: const EdgeInsets.all(18.0),
+    return AnimatedSize(
+      alignment: Alignment.topCenter,
+      curve: Curves.fastOutSlowIn,
+      duration: const Duration(milliseconds: 500),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Material(
             color: Colors.grey[400],
-            child: Row(
-              children: [
-                Text(
-                  title,
-                  style: textTheme.headline6?.copyWith(
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        const SizedBox(height: 4.0),
-        if (isExpanded)
-          for (final i in items)
-            InkWell(
-              onTap: () {
-                // TODO: Navigate to a certain route
-              },
-              child: Ink(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(
-                  vertical: 24.0,
-                  horizontal: 18.0,
-                ),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      width: 1.0,
-                      color: lightGrey,
+            child: InkWell(
+              onTap: onTap,
+              child: Container(
+                height: 96,
+                padding: const EdgeInsets.all(18.0),
+                child: Row(
+                  children: [
+                    Text(
+                      title,
+                      style: textTheme.headline6?.copyWith(
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ),
-                child: Text(
-                  i,
-                  style: textTheme.subtitle1?.copyWith(
-                    fontWeight: FontWeight.w500,
-                  ),
+                  ],
                 ),
               ),
             ),
-      ],
+          ),
+          Visibility(
+            visible: !isExpanded,
+            child: const SizedBox(height: 4.0),
+          ),
+          if (isExpanded)
+            for (final i in items)
+              InkWell(
+                onTap: () {
+                  // TODO: Navigate to a certain route
+                },
+                child: Ink(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 24.0,
+                    horizontal: 18.0,
+                  ),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1.0,
+                        color: lightGrey,
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    i,
+                    style: textTheme.subtitle1?.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+        ],
+      ),
     );
   }
 }
